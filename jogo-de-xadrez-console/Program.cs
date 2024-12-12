45865f0adf537981fd30dfb0ffa25e4dad5c7761
Gabriel Cruz <gabriel.araujo@tre-se.jus.br>
2024-12-12 09:12:08 -0300

    feat: ✨ Começando a implementar metodo para imprimir o tabuleiro.

--- jogo-de-xadrez-console/Program.cs
+++ jogo-de-xadrez-console/Program.cs
@@ -9,7 +9,7 @@ namespace jogo_de_xadrez_console
         {
           Tabuleiro tab = new Tabuleiro(8, 8);
 
-            
+          Tela.imprimirTabueleiro(tab);
 
           Console.ReadLine();
         }
