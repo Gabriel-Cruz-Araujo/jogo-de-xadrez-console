f8a6e44cd996ad63a3037e852be8fec22b9a1bf5
Gabriel Cruz <gabriel.araujo@tre-se.jus.br>
2024-12-12 08:50:43 -0300

    feat: ✨ Implementando a classe Posição.

--- jogo-de-xadrez-console/Program.cs
+++ jogo-de-xadrez-console/Program.cs
@@ -1,11 +1,16 @@
-﻿namespace jogo_de_xadrez_console
+﻿using System;
+using tabuleiro;
+
+namespace jogo_de_xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-             
-                
+            Posicao P;
+
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posicao " + P);
         }
     }
 }
