8363874be9bcf227129d8e622f3b28f4e3838bce
Gabriel Cruz <gabriel.araujo@tre-se.jus.br>
2024-12-12 09:53:33 -0300

    refactor:  ♻️ ajustando a implementação do metodo imprimir tabuleiro.

--- jogo-de-xadrez-console/Program.cs
+++ jogo-de-xadrez-console/Program.cs
@@ -9,27 +9,23 @@ namespace jogo_de_xadrez_console
     {
         static void Main(string[] args)
         {
-            //try
-            //{
-            //    //Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //    //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //    //tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //    //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
 
-
-
-            //    //Tela.imprimirTabueleiro(tab);
-            //}
-            //catch(TabuleiroException e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
-            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-            Console.WriteLine(pos);
-
-            Console.WriteLine(pos.toPosicao());
+                Tela.imprimirTabueleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
