ea3ed677e297ed0b17059f7882d3cf51059b6f8a
Gabriel Cruz <gabriel.araujo@tre-se.jus.br>
2024-12-12 09:23:35 -0300

    feat: ✨ colocando as peças no tabuleiro.

--- jogo-de-xadrez-console/Program.cs
+++ jogo-de-xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace jogo_de_xadrez_console
 {
@@ -9,7 +10,14 @@ namespace jogo_de_xadrez_console
         {
           Tabuleiro tab = new Tabuleiro(8, 8);
 
-          Tela.imprimirTabueleiro(tab);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            
+
+
+
+            Tela.imprimirTabueleiro(tab);
 
           Console.ReadLine();
         }
